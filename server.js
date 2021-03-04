20b8addf95d475f2e51b200a969aa5af68b98952
Michael Pabon <michael@mac-mini.lan>
2021-03-04 00:21:02 -0500

    Added API routes

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db')
+
+// Connect Database
+connectDB();
 
 app.get('/', (req,res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
